2f2b15c98977d0392fa8164563a6fc2c00d15fef
Yadickson Soto <yadickson@gmail.com>
2020-02-21 00:48:32 -0300

    Se actualiza configuracion de passport

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,23 +1,42 @@
 'use strict'
 
-module.exports = function (app) {
-  const passport = require('passport')
-  const JwtStrategy = require('passport-jwt').Strategy
-  const constants = require('../constants/constants')
+const passport = require('passport')
+const JwtStrategy = require('passport-jwt').Strategy
+const LocalStrategy = require('passport-local').Strategy
+const constants = require('../constants/constants')
 
-  const opts = {}
-  opts.jwtFromRequest = function (req) {
-    return req.headers[constants.JWT]
+function configLocal () {
+  const opts = {
+    usernameField: 'user',
+    passwordField: 'pass',
+    session: false
   }
-  opts.secretOrKey = process.env.JWT_SECRET
-  opts.algorithms = [constants.JWT_ALGORITHM]
+
+  passport.use(
+    new LocalStrategy(opts, (user, pass, done) => {
+      return done(null, { user: user })
+    })
+  )
+}
+
+function configJwt () {
+  const opts = {
+    jwtFromRequest: function (req) {
+      return req.headers[constants.JWT]
+    },
+    secretOrKey: process.env.JWT_SECRET,
+    algorithms: [constants.JWT_ALGORITHM]
+  }
 
   passport.use(
     new JwtStrategy(opts, (payload, done) => {
-      console.log('ejecutando *callback verify* de estategia jwt', payload)
       return done(null, payload)
     })
   )
+}
 
+module.exports = function (app) {
+  configLocal()
+  configJwt()
   app.use(passport.initialize())
 }
