994d8ee0f21b7a41721db7b0dfd2ce0962d81a56
Yadickson Soto <yadickson.soto@basedos.cl>
2020-02-21 15:01:34 -0300

    Se agregan pruebas unitarias con mejor chequeo de errores

--- test/app.routes.spec.js
+++ test/app.routes.spec.js
@@ -2,40 +2,43 @@
 
 const sinon = require('sinon')
 const mock = require('mock-require')
+const expect = require('chai').expect
 
 describe('Test App Routes', function () {
+  var context = ''
   var app = {}
 
-  var initAuthRoute = sinon.fake()
-  var initBaseRoute = sinon.fake()
-  var initOneRoute = sinon.fake()
+  var initAuthRoute = sinon.spy()
+  var initBaseRoute = sinon.spy()
+  var initOneRoute = sinon.spy()
 
-  before(() => {
-    mock('../src/routes/auth.routes', initAuthRoute)
-    mock('../src/routes/base.routes', initBaseRoute)
-    mock('../src/routes/one.routes', initOneRoute)
-    require('../src/app.routes')(app)
-  })
+  mock('../src/routes/auth.routes', initAuthRoute)
+  mock('../src/routes/base.routes', initBaseRoute)
+  mock('../src/routes/one.routes', initOneRoute)
 
-  after(() => {
-    sinon.restore()
-  })
+  require('../src/app.routes')(context, app)
 
   describe('auth.routes', function () {
     it('Check if ./routes/auth.routes was called', function () {
-      initAuthRoute.calledOn(app)
+      expect(initAuthRoute.calledOnceWith(context, app)).to.equal(true)
+      expect(initAuthRoute.getCall(0).args).to.be.an('array')
+      expect(initAuthRoute.getCall(0).args).to.have.ordered.members([context, app])
     })
   })
 
   describe('base.routes', function () {
     it('Check if ./routes/base.routes was called', function () {
-      initBaseRoute.calledOn(app)
+      expect(initBaseRoute.calledOnceWith(context, app)).to.equal(true)
+      expect(initBaseRoute.getCall(0).args).to.be.an('array')
+      expect(initBaseRoute.getCall(0).args).to.have.ordered.members([context, app])
     })
   })
 
   describe('one.routes', function () {
     it('Check if ./routes/one.routes was called', function () {
-      initOneRoute.calledOn(app)
+      expect(initOneRoute.calledOnceWith(context, app)).to.equal(true)
+      expect(initOneRoute.getCall(0).args).to.be.an('array')
+      expect(initOneRoute.getCall(0).args).to.have.ordered.members([context, app])
     })
   })
 })
